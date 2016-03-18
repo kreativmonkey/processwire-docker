b4368f1b30b4208544aa9fcd36aee5ccfd3d23e3
kreativmonkey <kreativmonkey@freifunk-myk.de>
2016-03-18 10:26:04 +0100

    php-fpm and apache version

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@
 # updateted sections are: 
 # "Processwire Dowload Part"
 
-FROM nginx:latest
-
+FROM php:5.5-apache
 MAINTAINER kreativmonkey <webmaster@calyrium.org>
+
+RUN a2enmod rewrite expires
 
 # set recommended PHP.ini settings
 # see https://secure.php.net/manual/en/opcache.installation.php
@@ -17,26 +18,33 @@ MAINTAINER kreativmonkey <webmaster@calyrium.org>
 #		echo 'opcache.enable_cli=1'; \
 #	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
 
-
+#Install the Extensions we need
 RUN apt-get update && apt-get -y upgrade \
-	&& apt-get install -y pwgen curl unzip php5-fpm php5-mysql php-apc php5-cli \
+	&& apt-get install -y pwgen curl unzip php5-mysql php-apc php5-cli \
 	 	php5-curl php5-gd php5-mcrypt php5-intl php5-imap php5-tidy php5-imagick \
-		php5-memcache php5-xmlrpc php5-xsl php5-mysql 
-
-ADD nginx.conf /etc/nginx/nginx.conf
+		php5-memcache php5-xmlrpc php5-xsl php5-mysql \
+	&& rm -rf /var/lib/apt/lists/* \
+	&& mkdir -p /var/www
 
 # Download Processwire, check and install
 ENV PROCESSWIRE_STACKE master
 ENV PROCESSWIRE_SHA1 bfde25a27432509dd060ff39a4e5aa8a71666fac
-ENV WEB_PATH /usr/share/nginx/www
+ENV WEB_PATH /var/www/html
+
+VOLUME ${WEB_PATH}
 
 RUN curl -o processwire.zip -SL https://github.com/ryancramerdesign/ProcessWire/archive/${PROCESSWIRE_STACKE}.zip \
 	&& echo "$PROCESSWIRE_SHA1 *processwire.zip" | sha1sum -c - \
-	&& unzip processwire.zip -d ${WEB_PATH}/ \
+	&& unzip processwire.zip -d /usr/src/ \
 	&& rm processwire.zip \
-	&& mv ${WEB_PATH}/ProcessWire-${PROCESSWIRE_STACKE} ${WEB_PATH} \
-	&& mv ${WEB_PATH}/site-beginner ${WEB_PATH}/site \
-	&& rm -rf ${WEB_PATH}/www/site-* \
-	&& chown -R www-data:www-data ${WEB_PATH}
+	&& mv /usr/src/ProcessWire-${PROCESSWIRE_STACKE} /usr/src/processwire \
+	&& mv /usr/src/processwire/site-beginner /usr/src/processwire/site \
+	&& rm -rf /usr/src/processwire/site-* \
+	&& chown -R www-data:www-data /usr/src/processwire
 
-VOLUME ${WEB_PATH}/site
+COPY docker-entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+#start php
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["apache2-foreground"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,50 +3,50 @@ set -e
 
 if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 	if [ -n "$MYSQL_PORT_3306_TCP" ]; then
-		if [ -z "$WORDPRESS_DB_HOST" ]; then
-			WORDPRESS_DB_HOST='mysql'
+		if [ -z "$PROCESSWIRE_DB_HOST" ]; then
+			PROCESSWIRE_DB_HOST='mysql'
 		else
-			echo >&2 'warning: both WORDPRESS_DB_HOST and MYSQL_PORT_3306_TCP found'
-			echo >&2 "  Connecting to WORDPRESS_DB_HOST ($WORDPRESS_DB_HOST)"
+			echo >&2 'warning: both PROCESSWIRE_DB_HOST and MYSQL_PORT_3306_TCP found'
+			echo >&2 "  Connecting to PROCESSWIRE_DB_HOST ($PROCESSWIRE_DB_HOST)"
 			echo >&2 '  instead of the linked mysql container'
 		fi
 	fi
 
-	if [ -z "$WORDPRESS_DB_HOST" ]; then
-		echo >&2 'error: missing WORDPRESS_DB_HOST and MYSQL_PORT_3306_TCP environment variables'
+	if [ -z "$PROCESSWIRE_DB_HOST" ]; then
+		echo >&2 'error: missing PROCESSWIRE_DB_HOST and MYSQL_PORT_3306_TCP environment variables'
 		echo >&2 '  Did you forget to --link some_mysql_container:mysql or set an external db'
-		echo >&2 '  with -e WORDPRESS_DB_HOST=hostname:port?'
+		echo >&2 '  with -e PROCESSWIRE_DB_HOST=hostname:port?'
 		exit 1
 	fi
 
 	# if we're linked to MySQL and thus have credentials already, let's use them
-	: ${WORDPRESS_DB_USER:=${MYSQL_ENV_MYSQL_USER:-root}}
-	if [ "$WORDPRESS_DB_USER" = 'root' ]; then
-		: ${WORDPRESS_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+	: ${PROCESSWIRE_DB_USER:=${MYSQL_ENV_MYSQL_USER:-root}}
+	if [ "$PROCESSWIRE_DB_USER" = 'root' ]; then
+		: ${PROCESSWIRE_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
 	fi
-	: ${WORDPRESS_DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
-	: ${WORDPRESS_DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-wordpress}}
+	: ${PROCESSWIRE_DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
+	: ${PROCESSWIRE_DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-PROCESSWIRE}}
 
-	if [ -z "$WORDPRESS_DB_PASSWORD" ]; then
-		echo >&2 'error: missing required WORDPRESS_DB_PASSWORD environment variable'
-		echo >&2 '  Did you forget to -e WORDPRESS_DB_PASSWORD=... ?'
+	if [ -z "$PROCESSWIRE_DB_PASSWORD" ]; then
+		echo >&2 'error: missing required PROCESSWIRE_DB_PASSWORD environment variable'
+		echo >&2 '  Did you forget to -e PROCESSWIRE_DB_PASSWORD=... ?'
 		echo >&2
-		echo >&2 '  (Also of interest might be WORDPRESS_DB_USER and WORDPRESS_DB_NAME.)'
+		echo >&2 '  (Also of interest might be PROCESSWIRE_DB_USER and PROCESSWIRE_DB_NAME.)'
 		exit 1
 	fi
 
-	if ! [ -e index.php -a -e wp-includes/version.php ]; then
-		echo >&2 "WordPress not found in $(pwd) - copying now..."
+	if ! [ -e index.php -a -e wire/config.php ]; then
+		echo >&2 "PROCESSWIRE not found in $(pwd) - copying now..."
 		if [ "$(ls -A)" ]; then
 			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
 			( set -x; ls -A; sleep 10 )
 		fi
-		tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
-		echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
+		tar cf - --one-file-system -C /usr/src/PROCESSWIRE . | tar xf -
+		echo >&2 "Complete! PROCESSWIRE has been successfully copied to $(pwd)"
 		if [ ! -e .htaccess ]; then
 			# NOTE: The "Indexes" option is disabled in the php:apache base image
 			cat > .htaccess <<-'EOF'
-				# BEGIN WordPress
+				# BEGIN PROCESSWIRE
 				<IfModule mod_rewrite.c>
 				RewriteEngine On
 				RewriteBase /
@@ -55,92 +55,40 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 				RewriteCond %{REQUEST_FILENAME} !-d
 				RewriteRule . /index.php [L]
 				</IfModule>
-				# END WordPress
+				# END PROCESSWIRE
 			EOF
 			chown www-data:www-data .htaccess
 		fi
 	fi
 
-	# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
-
-	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
-	# https://github.com/docker-library/wordpress/issues/116
-	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
-	sed -ri 's/\r\n|\r/\n/g' wp-config*
-
-	if [ ! -e wp-config.php ]; then
-		awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-EOPHP
-		chown www-data:www-data wp-config.php
-	fi
-
-	# see http://stackoverflow.com/a/2705678/433558
-	sed_escape_lhs() {
-		echo "$@" | sed 's/[]\/$*.^|[]/\\&/g'
-	}
-	sed_escape_rhs() {
-		echo "$@" | sed 's/[\/&]/\\&/g'
-	}
-	php_escape() {
-		php -r 'var_export(('$2') $argv[1]);' "$1"
-	}
-	set_config() {
-		key="$1"
-		value="$2"
-		var_type="${3:-string}"
-		start="(['\"])$(sed_escape_lhs "$key")\2\s*,"
-		end="\);"
-		if [ "${key:0:1}" = '$' ]; then
-			start="^(\s*)$(sed_escape_lhs "$key")\s*="
-			end=";"
-		fi
-		sed -ri "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" wp-config.php
-	}
-
-	set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
-	set_config 'DB_USER' "$WORDPRESS_DB_USER"
-	set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
-	set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
+	echo "/**" | tee site/config.php
+ 	echo "* Installer: Database Configuration" | tee site/config.php
+ 	echo "* " | tee site/config.php
+	echo "*/" | tee site/config.php
+	echo "\$config->dbHost = '$PROCESSWIRE_DB_HOST';" | tee site/config.php
+	echo "\$config->dbName = '$PROCESSWIRE_DB_NAME';" | tee site/config.php
+	echo "\$config->dbUser = '$PROCESSWIRE_DB_USER';" | tee site/config.php
+	echo "\$config->dbPass = '$PROCESSWIRE_DB_PASSWORD';" | tee site/config.php
+	echo "\$config->dbPort = '3306';" | tee site/config.php
 
 	# allow any of these "Authentication Unique Keys and Salts." to be specified via
-	# environment variables with a "WORDPRESS_" prefix (ie, "WORDPRESS_AUTH_KEY")
+	# environment variables with a "PROCESSWIRE_" prefix (ie, "PROCESSWIRE_AUTH_KEY")
 	UNIQUES=(
-		AUTH_KEY
-		SECURE_AUTH_KEY
-		LOGGED_IN_KEY
-		NONCE_KEY
-		AUTH_SALT
-		SECURE_AUTH_SALT
-		LOGGED_IN_SALT
-		NONCE_SALT
+		USERAUTHSALT
 	)
 	for unique in "${UNIQUES[@]}"; do
-		eval unique_value=\$WORDPRESS_$unique
+		eval unique_value=\$PROCESSWIRE_$unique
 		if [ "$unique_value" ]; then
-			set_config "$unique" "$unique_value"
-		else
-			# if not specified, let's generate a random value
-			current_set="$(sed -rn "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
-			if [ "$current_set" = 'put your unique phrase here' ]; then
-				set_config "$unique" "$(head -c1M /dev/urandom | sha1sum | cut -d' ' -f1)"
-			fi
+			echo "\$config->userAuthSalt = $unique_value" | tee site/config.php
 		fi
 	done
 
-	if [ "$WORDPRESS_TABLE_PREFIX" ]; then
-		set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
-	fi
 
-	if [ "$WORDPRESS_DEBUG" ]; then
+	if [ "$PROCESSWIRE_DEBUG" ]; then
 		set_config 'WP_DEBUG' 1 boolean
 	fi
 
-	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
+	TERM=dumb php -- "$PROCESSWIRE_DB_HOST" "$PROCESSWIRE_DB_USER" "$PROCESSWIRE_DB_PASSWORD" "$PROCESSWIRE_DB_NAME" <<'EOPHP'
 <?php
 // database might not exist, so let's try creating it (just to be safe)
 $stderr = fopen('php://stderr', 'w');
